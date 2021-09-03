39e55c8f7f845a275aee2609cefa59a3bc4ff045
npolyak <nick.polyak@verizon.net>
2021-09-03 02:18:56 -0400

    converters sample

---
+++ NP.Demos.AvaloniaConcepts/NP.Demos.BindingConvertersSample/ReverseStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace NP.Demos.BindingConvertersSample
+{
+    public class ReverseStringConverter : IValueConverter
+    {
+        private static string? ReverseStr(object value)
+        {
+            if (value is string str)
+            {
+                return new string(str.Reverse().ToArray());
+            }
+
+            return null;
+        }
+
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ReverseStr(value);
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ReverseStr(value);
+        }
+    }
+}
