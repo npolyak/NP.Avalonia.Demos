8eab4890d8e1e50ccdd8605b031ea23d082df533
npolyak <nick.polyak@verizon.net>
2021-09-30 13:44:33 -0400

    added a project showing how to use Assets in XAML

---
+++ NP.Demos.XamlSamples/NP.Demos.ReferringToAssetsInXaml/MainProject/MainWindow.axaml.cs
@@ -0,0 +1,36 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
+
+namespace NP.Demos.ReferringToAssetsInXaml
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var assetsLoader = AvaloniaLocator.Current.GetService<IAssetLoader>();
+
+            Image linuxIconImage2 = this.FindControl<Image>("LinuxIconImage2");
+            linuxIconImage2.Source = 
+                new Bitmap(assetsLoader.Open
+                           (new Uri("avares://NP.Demos.ReferringToAssetsInXaml/Assets/LinuxIcon.jpg")));
+
+            Image avaloniaIconImage2 = this.FindControl<Image>("AvaloniaIconImage2");
+            avaloniaIconImage2.Source =
+                new Bitmap(assetsLoader.Open
+                           (new Uri("avares://Dependency1Proj/Assets/avalonia-32.png")));
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
