d8113bcbcd678a2290d6e8e3c8dddb19d626704a
npolyak <nick.polyak@verizon.net>
2021-09-30 16:05:05 -0400

    minor changes

--- NP.Demos.XamlSamples/NP.Demos.NonVisualXamlSample/Program.cs
+++ NP.Demos.XamlSamples/NP.Demos.NonVisualXamlSample/Program.cs
@@ -3,12 +3,9 @@ namespace NP.Demos.NonVisualXamlSample
 {
     class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
         public static void Main(string[] args)
         {
-            Course nonVisualSample = new Course();
+            Course course = new Course();
         }
     }
 }
