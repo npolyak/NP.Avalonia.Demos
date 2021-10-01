85f4aa50c7cdbaa626170f21b1311d575d2da857
npolyak <nick.polyak@verizon.net>
2021-10-01 16:01:58 -0400

    added custom routed event sample

---
+++ NP.Demos.AvaloniaBasicConcepts/NP.Demos.CustomRoutedEventSample/MainWindow.axaml.cs
@@ -0,0 +1,76 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System.Diagnostics;
+
+namespace NP.Demos.CustomRoutedEventSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            // add event handler for the Window
+            this.AddHandler
+            (
+                StaticRoutedEvents.MyCustomRoutedEvent, //routed event
+                HandleClickEvent, // event handler
+                RoutingStrategies.Bubble | RoutingStrategies.Tunnel // routing strategy filter
+            );
+
+            Grid rootPanel = this.FindControl<Grid>("TheRootPanel");
+
+            // add event handler for the Grid
+            rootPanel.AddHandler
+            (
+                StaticRoutedEvents.MyCustomRoutedEvent, 
+                HandleClickEvent,
+                RoutingStrategies.Bubble | RoutingStrategies.Tunnel);
+
+            Border border = this.FindControl<Border>("TheBorder");
+
+            // add event handler for the Blue Border in the middle
+            border.AddHandler(
+                StaticRoutedEvents.MyCustomRoutedEvent,
+                HandleClickEvent,
+                RoutingStrategies.Bubble | RoutingStrategies.Tunnel
+                );
+
+            // we add the handler to pointer pressed event in order
+            // to raise MyCustomRoutedEvent from it.
+            border.PointerPressed += Border_PointerPressed;
+        }
+
+        private void Border_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            Control control = (Control)sender!;
+
+            // Raising MyCustomRoutedEvent
+            control.RaiseEvent(new RoutedEventArgs(StaticRoutedEvents.MyCustomRoutedEvent));
+        }
+
+        private void HandleClickEvent(object? sender, RoutedEventArgs e)
+        {
+            Control senderControl = (Control) sender!;
+
+            string eventTypeStr = e.Route switch
+            {
+                RoutingStrategies.Bubble => "Bubbling",
+                RoutingStrategies.Tunnel => "Tunneling",
+                _ => "Direct"
+            };
+
+            Debug.WriteLine($"{eventTypeStr} Routed Event {e.RoutedEvent!.Name} raised on {senderControl.Name}; Event Source is {(e.Source as Control)!.Name}");
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ NP.Demos.AvaloniaBasicConcepts/NP.Demos.CustomRoutedEventSample/StaticRoutedEvents.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Interactivity;
+
+namespace NP.Demos.CustomRoutedEventSample
+{
+    public static class StaticRoutedEvents
+    {
+        /// <summary>
+        /// define the MyCustomRoutedEvent
+        /// </summary>
+        public static readonly RoutedEvent<RoutedEventArgs> MyCustomRoutedEvent =
+            RoutedEvent.Register<object, RoutedEventArgs>("MyCustomRouted", RoutingStrategies.Tunnel);
+    }
+}
