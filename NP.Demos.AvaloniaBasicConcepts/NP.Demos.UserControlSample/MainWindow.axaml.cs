f0340d11e3442e29947dab17b58d3fb50a65439f
npolyak <nick.polyak@verizon.net>
2021-10-03 03:26:36 -0400

    added a sample

---
+++ NP.Demos.AvaloniaBasicConcepts/NP.Demos.UserControlSample/MainWindow.axaml.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Windows.Input;
+
+namespace NP.Demos.UserControlSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ NP.Demos.AvaloniaBasicConcepts/NP.Demos.UserControlSample/MyUserControl.axaml.cs
@@ -0,0 +1,61 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System;
+
+namespace NP.Demos.UserControlSample
+{
+    public partial class MyUserControl : UserControl
+    {
+        private TextBox _textBox;
+        private Button _cancelButton;
+        private Button _saveButton;
+
+        private string? _oldValue = null;
+
+        private string? NewValue
+        {
+            get => _textBox.Text;
+            set => _textBox.Text = value;
+        }
+
+        public MyUserControl()
+        {
+            InitializeComponent();
+
+            _cancelButton = this.FindControl<Button>("CancelButton");
+            _cancelButton.Click += _cancelButton_Click;
+
+            _saveButton = this.FindControl<Button>("SaveButton");
+            _saveButton.Click += _saveButton_Click;
+
+
+            _textBox = this.FindControl<TextBox>("TheTextBox");
+
+            _textBox.GetObservable(TextBox.TextProperty).Subscribe(OnTextChanged);
+        }
+
+        private void _cancelButton_Click(object? sender, RoutedEventArgs e)
+        {
+            NewValue = _oldValue;
+        }
+
+        private void _saveButton_Click(object? sender, RoutedEventArgs e)
+        {
+            _oldValue = NewValue;
+        }
+
+        private void OnTextChanged(string obj)
+        {
+            bool canSave = NewValue != _oldValue;
+            _cancelButton.IsEnabled = canSave;
+            _saveButton.IsEnabled = canSave;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
