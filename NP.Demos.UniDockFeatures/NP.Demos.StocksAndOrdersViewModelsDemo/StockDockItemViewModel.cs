463e398444e2537cf51a2a856793d54813a9f1e3
npolyak <nick.polyak@verizon.net>
2021-11-02 23:21:39 -0400

    added a sample

---
+++ NP.Demos.UniDockFeatures/NP.Demos.StocksAndOrdersViewModelsDemo/StockDockItemViewModel.cs
@@ -0,0 +1,8 @@
+﻿using NP.Avalonia.UniDockService;
+
+namespace NP.Demos.StocksAndOrdersViewModelsDemo
+{
+    public class StockDockItemViewModel : DockItemViewModel<StockViewModel>
+    {
+    }
+}
