fd6dbba6cb6542d6e5994f8fa84475e72c411d32
npolyak <nick.polyak@verizon.net>
2021-11-17 18:33:19 -0500

    added theming/localization example

---
+++ NP.Demos.ThemingAndL10N/NP.Demos.ThemingAndLocalizationDemo/Data.cs
@@ -0,0 +1,43 @@
+﻿using NP.Utilities;
+
+namespace NP.Demos.ThemingAndLocalizationDemo
+{
+    public class Data : VMBase
+	{
+		private static int _sid = 0;
+
+		private int _id = ++_sid;
+
+		public int ID
+		{
+			get { return _id; }
+		}
+
+
+        #region Uid Property
+        private string _uid;
+        public string Uid
+        {
+            get
+            {
+                return this._uid;
+            }
+            set
+            {
+                if (this._uid == value)
+                {
+                    return;
+                }
+
+                this._uid = value;
+                this.OnPropertyChanged(nameof(Uid));
+            }
+        }
+        #endregion Uid Property
+
+        public Data(string uid)
+		{
+			this._uid = uid;
+		}
+	}
+}
---
+++ NP.Demos.ThemingAndL10N/NP.Demos.ThemingAndLocalizationDemo/Language.cs
@@ -0,0 +1,19 @@
+﻿namespace NP.Demos.ThemingAndLocalizationDemo
+{
+    public enum Language
+    {
+        English,
+        Hebrew,
+        Russian
+    }
+
+    public static class LanguageHelper
+    {
+        public static Language[] Languages { get; } =
+        {
+            Language.English,
+            Language.Hebrew,
+            Language.Russian
+        };
+    }
+}
