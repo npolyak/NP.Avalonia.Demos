c84cb15879ac5fbcab9cbbd726a50e53cdfa5b32
npolyak <nick.polyak@verizon.net>
2021-11-18 01:29:54 -0500

    rearranged code a little bit. addes some comments

--- NP.Demos.ThemingAndL10N/NP.Demos.SimpleThemingSample/MainWindow.axaml.cs
+++ NP.Demos.ThemingAndL10N/NP.Demos.SimpleThemingSample/MainWindow.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using NP.Avalonia.Visuals.Behaviors;
 using NP.Avalonia.Visuals.ThemingAndL10N;
@@ -8,6 +9,8 @@ namespace NP.Demos.SimpleThemingSample
 {
     public partial class MainWindow : Window
     {
+        // reference to ThemeLoader object defined
+        // in XAML
         private ThemeLoader _themeLoader;
 
         public MainWindow()
@@ -16,24 +19,26 @@ namespace NP.Demos.SimpleThemingSample
 #if DEBUG
             this.AttachDevTools();
 #endif
+            // find the theme loader by its name
+            _themeLoader =
+                Application.Current.Resources.GetThemeLoader("ColorThemeLoader")!;
+
+            // set the handler for lightButton's click event
             Button lightButton = this.FindControl<Button>("LightButton");
             lightButton.Click += LightButton_Click;
 
+            // set the handler for darkButton's click event
             Button darkButton = this.FindControl<Button>("DarkButton");
             darkButton.Click += DarkButton_Click;
-
-            // find the theme loader by name
-            _themeLoader =
-                Application.Current.Resources.GetThemeLoader("ColorThemeLoader")!;
         }
 
-        private void LightButton_Click(object? sender, global::Avalonia.Interactivity.RoutedEventArgs e)
+        private void LightButton_Click(object? sender, RoutedEventArgs e)
         {
             // set the theme to Light
             _themeLoader.SelectedThemeId = "Light";
         }
 
-        private void DarkButton_Click(object? sender, global::Avalonia.Interactivity.RoutedEventArgs e)
+        private void DarkButton_Click(object? sender, RoutedEventArgs e)
         {
             // set the theme to Dark
             _themeLoader.SelectedThemeId = "Dark";
