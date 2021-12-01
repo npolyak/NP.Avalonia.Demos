72e503709d99e6baad6ff71b4191a0b4a42c1e8e
npolyak <nick.polyak@verizon.net>
2021-12-01 18:17:22 -0500

    added generics sample

---
+++ NP.Demos.XamlSamples/NP.Demos.XamlGenericsSamples/MainProject/ValuesContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace NP.Demos.XamlGenericsSamples
+{
+    public class ValuesContainer<TVal1, TVal2>
+    {
+        public TVal1? Value1 { get; set; }
+
+        public TVal2? Value2 { get; set; }
+    }
+}
