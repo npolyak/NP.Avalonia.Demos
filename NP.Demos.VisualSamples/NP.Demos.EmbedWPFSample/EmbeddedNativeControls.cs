3f7d601e611896c642f3c533370f62c021e809dc
npolyak <nick.polyak@verizon.net>
2021-12-14 20:30:53 -0500

    added WPF control embedded in Avalonia sample

---
+++ NP.Demos.VisualSamples/NP.Demos.EmbedWPFSample/EmbeddedNativeControls.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Platform;
+
+#if WINDOWS
+using WpfControl;
+#endif
+
+namespace NP.Demos.EmbedWinFormsSample
+{
+    public class EmbeddedNativeControls : NativeControlHost
+    {
+        protected override IPlatformHandle CreateNativeControlCore(IPlatformHandle parent)
+        {
+#if WINDOWS
+            return new PlatformHandle(SimpleUserControl.GetElementHost(), "CTRL");
+#else
+            return base.CreateNativeControlCore(parent);
+#endif
+        }
+
+        protected override void DestroyNativeControlCore(IPlatformHandle control)
+        {
+            base.DestroyNativeControlCore(control);
+        }
+    }
+}
---
+++ NP.Demos.VisualSamples/NP.Demos.EmbedWPFSample/PlatformSpecific/WpfControl/SimpleUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Forms.Integration;
+using System.Windows.Interop;
+
+namespace WpfControl
+{
+    /// <summary>
+    /// Interaction logic for SimpleUserControl.xaml
+    /// </summary>
+    public partial class SimpleUserControl : UserControl
+    {
+        public SimpleUserControl()
+        {
+            InitializeComponent();
+        }
+
+        public static IntPtr GetElementHost()
+        {
+            ElementHost host = 
+                new ElementHost() { Child = new SimpleUserControl() };
+
+            return host.Handle;
+        }
+    }
+}
