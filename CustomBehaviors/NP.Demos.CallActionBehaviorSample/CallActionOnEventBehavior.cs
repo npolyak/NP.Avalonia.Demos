4f789acff37f7d202504fe88b8afec3461ec31ae
npolyak <nick.polyak@verizon.net>
2021-12-23 13:50:22 -0500

    Avalonia behavior test

---
+++ CustomBehaviors/NP.Demos.CallActionBehaviorSample/CallActionOnEventBehavior.cs
@@ -0,0 +1,124 @@
+﻿using Avalonia.Interactivity;
+using Avalonia.Controls;
+using Avalonia;
+using System;
+using System.Reflection;
+
+namespace NP.Demos.CallActionBehaviorSample
+{
+    public class CallActionOnEventBehavior
+    {
+        #region TheEvent Attached Avalonia Property
+        public static RoutedEvent GetTheEvent(IControl obj)
+        {
+            return obj.GetValue(TheEventProperty);
+        }
+
+        public static void SetTheEvent(IControl obj, RoutedEvent value)
+        {
+            obj.SetValue(TheEventProperty, value);
+        }
+
+        public static readonly AttachedProperty<RoutedEvent> TheEventProperty =
+            AvaloniaProperty.RegisterAttached<CallActionOnEventBehavior, IControl, RoutedEvent>
+            (
+                "TheEvent"
+            );
+        #endregion TheEvent Attached Avalonia Property
+
+
+        #region TargetObject Attached Avalonia Property
+        public static object GetTargetObject(IControl obj)
+        {
+            return obj.GetValue(TargetObjectProperty);
+        }
+
+        public static void SetTargetObject(IControl obj, object value)
+        {
+            obj.SetValue(TargetObjectProperty, value);
+        }
+
+        public static readonly AttachedProperty<object> TargetObjectProperty =
+            AvaloniaProperty.RegisterAttached<CallActionOnEventBehavior, IControl, object>
+            (
+                "TargetObject"
+            );
+        #endregion TargetObject Attached Avalonia Property
+
+
+        #region MethodToCall Attached Avalonia Property
+        public static string GetMethodToCall(IControl obj)
+        {
+            return obj.GetValue(MethodToCallProperty);
+        }
+
+        public static void SetMethodToCall(IControl obj, string value)
+        {
+            obj.SetValue(MethodToCallProperty, value);
+        }
+
+        public static readonly AttachedProperty<string> MethodToCallProperty =
+            AvaloniaProperty.RegisterAttached<CallActionOnEventBehavior, IControl, string>
+            (
+                "MethodToCall"
+            );
+        #endregion MethodToCall Attached Avalonia Property
+
+        static CallActionOnEventBehavior()
+        {
+            TheEventProperty.Changed.Subscribe(OnEventChanged);
+        }
+
+        private static void OnEventChanged(AvaloniaPropertyChangedEventArgs<RoutedEvent> args)
+        {
+            IControl el = (IControl) args.Sender;
+
+            RoutedEvent? oldRoutedEvent = args.OldValue.Value as RoutedEvent;
+
+            if (oldRoutedEvent != null)
+            {
+                // remove old event handler from the object (if exists)
+                el.RemoveHandler(oldRoutedEvent, (EventHandler<RoutedEventArgs>)HandleRoutedEvent!);
+            }
+
+
+            RoutedEvent newRoutedEvent = args.NewValue.Value as RoutedEvent;
+
+            if (newRoutedEvent != null)
+            {
+                // add new event handler to the object
+                el.AddHandler(newRoutedEvent, (EventHandler<RoutedEventArgs>)HandleRoutedEvent!);
+            }
+        }
+
+        // handle the routed event when happens on the object
+        // by calling the method of name 'methodName' onf the
+        // TargetObject
+        private static void HandleRoutedEvent(object sender, RoutedEventArgs e)
+        {
+            IControl el = (IControl)sender;
+
+            // if TargetObject is not set, use DataContext as the target object
+            object? targetObject = GetTargetObject(el) ?? el.DataContext;
+
+            string? methodName = GetMethodToCall(el);
+
+            // do not do anything
+            if (targetObject == null || methodName == null)
+            {
+                return;
+            }
+
+            MethodInfo? methodInfo =
+                targetObject.GetType().GetMethod(methodName);
+
+            if (methodInfo == null)
+            {
+                return;
+            }
+
+            // call the method using reflection
+            methodInfo.Invoke(targetObject, null);
+        }
+    }
+}
