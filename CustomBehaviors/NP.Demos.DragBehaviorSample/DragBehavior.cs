38af30f5b4ba9b54d408926e4c5cac382805af0b
npolyak <nick.polyak@verizon.net>
2021-12-23 21:09:05 -0500

    added drag behavior

---
+++ CustomBehaviors/NP.Demos.DragBehaviorSample/DragBehavior.cs
@@ -0,0 +1,157 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.VisualTree;
+using System;
+using System.Linq;
+
+namespace NP.Demos.DragBehaviorSample
+{
+    public class DragBehavior
+    {
+        #region IsSet Attached Avalonia Property
+        public static bool GetIsSet(IControl obj)
+        {
+            return obj.GetValue(IsSetProperty);
+        }
+
+        public static void SetIsSet(IControl obj, bool value)
+        {
+            obj.SetValue(IsSetProperty, value);
+        }
+
+        public static readonly AttachedProperty<bool> IsSetProperty =
+            AvaloniaProperty.RegisterAttached<DragBehavior, IControl, bool>
+            (
+                "IsSet"
+            );
+        #endregion IsSet Attached Avalonia Property
+
+
+        #region StartPointerLocation Attached Avalonia Property
+        private static Point GetStartPointerLocation(IControl obj)
+        {
+            return obj.GetValue(StartPointerLocationProperty);
+        }
+
+        private static void SetStartPointerLocation(IControl obj, Point value)
+        {
+            obj.SetValue(StartPointerLocationProperty, value);
+        }
+
+        private static readonly AttachedProperty<Point> StartPointerLocationProperty =
+            AvaloniaProperty.RegisterAttached<DragBehavior, IControl, Point>
+            (
+                "StartPointerLocation"
+            );
+        #endregion StartPointerLocation Attached Avalonia Property
+
+
+        #region StartControlPosition Attached Avalonia Property
+        private static Point GetStartControlPosition(IControl obj)
+        {
+            return obj.GetValue(StartControlPositionProperty);
+        }
+
+        private static void SetStartControlPosition(IControl obj, Point value)
+        {
+            obj.SetValue(StartControlPositionProperty, value);
+        }
+
+        private static readonly AttachedProperty<Point> StartControlPositionProperty =
+            AvaloniaProperty.RegisterAttached<DragBehavior, IControl, Point>
+            (
+                "StartControlPosition"
+            );
+        #endregion StartControlPosition Attached Avalonia Property
+
+
+        static DragBehavior()
+        {
+            IsSetProperty.Changed.Subscribe(OnIsSetChanged);
+        }
+
+        private static void OnIsSetChanged(AvaloniaPropertyChangedEventArgs<bool> args)
+        {
+            IControl control = (IControl) args.Sender;
+
+            if (args.NewValue.Value == true)
+            {
+                control.RenderTransform = new TranslateTransform();
+                control.PointerPressed += Control_PointerPressed;
+            }
+            else
+            {
+                control.RenderTransform = null;
+                control.PointerPressed -= Control_PointerPressed;
+            }
+        }
+
+        private static Window GetWindow(IControl control)
+        {
+            return control.GetVisualAncestors().OfType<Window>().FirstOrDefault()!;
+        }
+
+        public static Point GetCurrentPointerPositionInWindow(IControl control, PointerEventArgs e)
+        {
+            return e.GetPosition(GetWindow(control));
+        }
+
+        private static void Control_PointerPressed(object? sender, PointerPressedEventArgs e)
+        {
+            IControl control = (IControl)sender!;
+            e.Pointer.Capture(control);
+
+            Window w = GetWindow(control);
+
+            Point currentPointerPositionInWindow = GetCurrentPointerPositionInWindow(control, e);
+            
+            SetStartPointerLocation(control, currentPointerPositionInWindow);
+
+            Point startControlPosition = 
+                new Point
+                (
+                    (control.RenderTransform as TranslateTransform)!.X,
+                    (control.RenderTransform as TranslateTransform)!.Y);
+
+            SetStartControlPosition(control, startControlPosition);
+
+            control.PointerMoved += Control_PointerMoved;
+            control.PointerReleased += Control_PointerReleased;
+        }
+
+        private static void Control_PointerReleased(object? sender, PointerReleasedEventArgs e)
+        {
+            IControl control = (IControl)sender!;
+
+            e.Pointer.Capture(null);
+
+            ShiftControl(control, e);
+            control.PointerMoved -= Control_PointerMoved;
+            control.PointerReleased -= Control_PointerReleased;
+        }
+
+        private static void Control_PointerMoved(object? sender, PointerEventArgs e)
+        {
+            IControl control = (IControl)sender!;
+            ShiftControl(control, e);
+        }
+
+        private static void ShiftControl(IControl control, PointerEventArgs e)
+        {
+            Point currentPointerPosition = GetCurrentPointerPositionInWindow(control, e);
+
+            Point startPointerPosition = GetStartPointerLocation(control);
+
+            Point diff = currentPointerPosition - startPointerPosition;
+
+            Point startControlPosition = GetStartControlPosition(control);
+
+            Point shift = diff + startControlPosition;
+
+            (control.RenderTransform as TranslateTransform).X = shift.X;
+            (control.RenderTransform as TranslateTransform).Y = shift.Y;
+        }
+    }
+}
---
+++ CustomBehaviors/NP.Demos.DragBehaviorSample/MainWindow.axaml.cs
@@ -0,0 +1,54 @@
+﻿using Avalonia.Layout;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia;
+
+namespace NP.Demos.DragBehaviorSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        // Turns window background red
+        public void MakeWindowBackgroundRed()
+        {
+            Background = new SolidColorBrush(Colors.Red);
+        }
+
+        // opens a dialog
+        public void OpenDialog()
+        {
+            Window dialogWindow =
+                new Window()
+                {
+                    Position = new PixelPoint(this.Position.X + 50, this.Position.Y + 50),
+                    Width = 200,
+                    Height = 200
+                };
+
+            dialogWindow.Content = new TextBlock
+            {
+
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Text = "I am a happy dialog!"
+            };
+
+            dialogWindow.ShowDialog(this);
+        }
+    }
+}
