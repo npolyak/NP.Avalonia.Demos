d726ed27489655a6fba1fbce70d00a214924bfba
npolyak <nick.polyak@verizon.net>
2022-04-11 00:54:13 -0400

    added a demo project for advanced grid functionality

---
+++ NP.Demos.VisualSamples/NP.Demos.AdvancedDataGridDemo/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace NP.Demos.AdvancedDataGridDemo
+{
+    public class Product
+    {
+        public string? Name { get; }
+
+        public string? Description { get; }
+
+        public string? Manufacturer { get; }
+
+        public double? Cost { get; }
+
+
+        public Product(string? name, string? description, string? manufacturer, double? cost)
+        {
+            Name = name;
+            Description = description;  
+            Manufacturer = manufacturer;
+            Cost = cost;
+        }
+    }
+}
