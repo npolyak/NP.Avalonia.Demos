409133ce8d839f8fadb40c85eb915a8036f9aa0e
npolyak <nick.polyak@verizon.net>
2022-04-19 17:31:00 -0400

    Debuggon .NET Console app on Linux sample

---
+++ ConsileAppForLinuxSubsystemDebuggingDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+// Various ways to get information about the OS
+
+bool isLinux = OperatingSystem.IsLinux();
+
+if (isLinux)
+{
+    Console.WriteLine("Yes, it is Linux!!!");
+}
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+{
+    Console.WriteLine("Yes, indeed Linux!!!");
+}
+
+Console.WriteLine($"RuntimeInformation.OSDescription = '{RuntimeInformation.OSDescription}'");
+
+// gives Linux type, version number and architecture:
+Console.WriteLine($"RuntimeInformation.RuntimeIdentifier = '{RuntimeInformation.RuntimeIdentifier}'");
+
+Console.WriteLine($"Environment.OSVersion = '{Environment.OSVersion}'");
