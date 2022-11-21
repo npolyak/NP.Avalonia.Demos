448ec08bcb8df94adbadacec554e86575b095ca1
npolyak <nick.polyak@verizon.net>
2022-11-21 16:38:34 -0500

    initial project

---
+++ HostingDemos/HostingWindowsProcessDemo/HostingWindowsProcessDemo/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HostingWindowsProcessDemo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
