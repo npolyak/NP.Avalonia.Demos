11f43159787b67af8be2eab94e84b0e0ce9fa478
Artem <tiomny@yandex.ru>
2022-12-08 10:41:52 +0600

    Added demo for non-removable columns

---
+++ NP.Demos.VisualSamples/NP.Demos.AdvancedDataGridFilteringNonRemovableDemo/People.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace NP.DataGridFilteringDemo
+{
+    public class People : ObservableCollection<Person>
+    {
+        public void AddPerson(string firstName, string lastName)
+        {
+            this.Add(new Person { FirstName = firstName, LastName = lastName });    
+        }
+
+        public People()
+        {
+            AddPerson("John", "Plantagenet");
+            AddPerson("Richard", "Plantagenet");
+            AddPerson("Henry", "Tudor");
+            AddPerson("Elizabeth", "Tudor");
+        }
+    }
+}
