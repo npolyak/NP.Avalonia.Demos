4d77507429e7a6adfbf6134ea7338b4b6158eb07
Philippe Laval <philippe.laval.1968@laposte.net>
2023-08-22 13:26:00 +0200

    Upgrade project

--- NP.Demos.XamlSamples/NP.Demos.ReferringToAssetsInXaml/MainProject/MainWindow.axaml.cs
+++ NP.Demos.XamlSamples/NP.Demos.ReferringToAssetsInXaml/MainProject/MainWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using NP.Demos.ReferringToAssetsInXaml.Helpers;
 using System;
 
 namespace NP.Demos.ReferringToAssetsInXaml
@@ -15,28 +16,26 @@ namespace NP.Demos.ReferringToAssetsInXaml
 #if DEBUG
             this.AttachDevTools();
 #endif
-            // get the asset loader from Avalonia container
-            var assetLoader = AvaloniaLocator.Current.GetService<IAssetLoader>();
+            // get the Image control from XAML
+            Image? linuxIconImage2 = this.FindControl<Image>("LinuxIconImage2");
+
+            if (linuxIconImage2 is not null)
+            {
+                // set the image Source using assetLoader
+                linuxIconImage2.Source =
+                    new Bitmap
+                    (
+                        AssetLoader.Open(
+                            new Uri("avares://NP.Demos.ReferringToAssetsInXaml/Assets/LinuxIcon.jpg")));
+            }
 
             // get the Image control from XAML
-            Image linuxIconImage2 = this.FindControl<Image>("LinuxIconImage2");
-
-            // set the image Source using assetLoader
-            linuxIconImage2.Source = 
-                new Bitmap
-                (
-                    assetLoader.Open(
-                        new Uri("avares://NP.Demos.ReferringToAssetsInXaml/Assets/LinuxIcon.jpg")));
-
-            // get the Image control from XAML
-            Image avaloniaIconImage2 = this.FindControl<Image>("AvaloniaIconImage2");
-
-            // set the image Source using assetLoader
-            avaloniaIconImage2.Source =
-                new Bitmap
-                (
-                    assetLoader.Open(
-                        new Uri("avares://Dependency1Proj/Assets/avalonia-32.png")));
+            Image? avaloniaIconImage2 = this.FindControl<Image>("AvaloniaIconImage2");
+            if (avaloniaIconImage2 is not null)
+            {
+                // set the image Source using assetLoader
+                avaloniaIconImage2.Source = ImageHelper.LoadFromResource(new Uri("avares://Dependency1Proj/Assets/avalonia-32.png"));
+            }
         }
 
         private void InitializeComponent()
